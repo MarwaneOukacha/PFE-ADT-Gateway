09b14ec7ec9bce6693beea04206dadd92cab211a
Mohamed_BENMANSOUR <mohamed.benmansour@adria-bt.com>
2024-03-12 14:48:44 +0000

    Refactor security config and update project dependencies
    The commit refactors the SecurityConfig by declaring public resources as an array, for cleaner and more efficient code. In the pom.xml file, project dependencies have been rearranged, and some new dependencies were introduced. The SwaggerConfig file was also updated with contact and version information. Changes in the application.yml file include setting the context-path and enabling swagger-ui path.

--- src/main/java/ma/adria/document_validation/gateway/config/SecurityConfig.java
+++ src/main/java/ma/adria/document_validation/gateway/config/SecurityConfig.java
@@ -3,23 +3,28 @@ package ma.adria.document_validation.gateway.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
-import org.springframework.security.config.annotation.web.servlet.configuration.EnableWebMvcSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableWebFluxSecurity
-public class SecurityConfig{
-	@Bean
-	public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity serverHttpSecurity) {
-		return serverHttpSecurity.csrf(ServerHttpSecurity.CsrfSpec::disable)
-				.authorizeExchange(exchage->exchage.pathMatchers("/login/**","/swagger-ui/**","/api-docs/**","/api/**","/swagger-ui.html").permitAll()
-				.anyExchange().authenticated())
-				.oauth2ResourceServer((oauth)->oauth.jwt(Customizer.withDefaults())).build();
-	}
-	
+public class SecurityConfig {
+
+    private static final String[] PUBLIC_RESOURCES = {
+            "/login/**",
+            "/api-docs/**",
+            "/swagger-ui/**",
+            "/api/**"
+    };
+
+    @Bean
+    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity serverHttpSecurity) {
+        return serverHttpSecurity.csrf(ServerHttpSecurity.CsrfSpec::disable)
+                .authorizeExchange(exchange ->
+                        exchange.pathMatchers(PUBLIC_RESOURCES).permitAll()
+                                .anyExchange().authenticated()
+                ).oauth2ResourceServer(oauth -> oauth.jwt(Customizer.withDefaults())).build();
+    }
+
 }
